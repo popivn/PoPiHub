fb6984c4403c43b391653c6fc1b34ca7b7ce5402
atech <dev@atech88.com>
2026-08-06 14:46:09 +0700

    update

--- popihub-upload/src/main.ts
+++ popihub-upload/src/main.ts
@@ -6,10 +6,13 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setBaseViewsDir(join(__dirname, 'views'));
   app.setViewEngine('hbs');
 
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application running on port ${process.env.PORT ?? 3000}`);
+  const port = process.env.PORT ?? 1602;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application running on:
+  Local:   http://localhost:${port}
+  Network: http://192.168.1.108:${port}`);
 }
 bootstrap();
